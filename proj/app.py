47f1d22b924a42d209afa4d42c3532ab0e027c8f
WrongEnd <jacobsroom@hotmail.com>
2018-06-24 22:56:49 -0500

    help im dumb

--- proj/app.py
+++ proj/app.py
@@ -1,6 +1,6 @@
+from config_parser import Config
 from flask import Flask, request
 
-from config_parser import Config
 
 app = Flask(__name__)
 
@@ -14,6 +14,7 @@ def get_env():
     else:
         return 'Please enter a god\'s name from greek mythology!'
 
+
 if __name__ == "__main__":
-	MAIN_CONFIG = Config.parse("config/main_config.config")
-	app.run(host = MAIN_CONFIG["IP"], port = MAIN_CONFIG["PORT"])
+    MAIN_CONFIG = Config.parse("config/main_config.config")
+    app.run(host = MAIN_CONFIG["IP"], port = MAIN_CONFIG["PORT"])
--- proj/config_parser.py
+++ proj/config_parser.py
@@ -1,14 +1,14 @@
 class Config:
-	def parse(file_loc):
-		main_data = {}
-		with open(file_loc, 'r') as main_file:
-			for line in main_file.readlines():
-				clean_line = line.replace(" ", "").rstrip()
-				if clean_line.startswith('#') or len(clean_line) == 0:
-					continue
-				if clean_line.find('=') == -1:
-					raise SyntaxError("Config missing = on a line.") #Todo: make more informative.
-				splitted = clean_line.split("=")
-				main_data[splitted[0]] = splitted[1] #Todo: make more robust.
-		return main_data
+    def parse(file_loc):
+        main_data = {}
+        with open(file_loc, 'r') as main_file:
+            for line in main_file.readlines():
+                clean_line = line.replace(" ", "").rstrip()
+                if clean_line.startswith('#') or len(clean_line) == 0:
+                    continue
+                if clean_line.find('=') == -1:
+                    raise SyntaxError("Config missing = on a line.")  # Todo: make more informative.
+                splitted = clean_line.split("=")
+                main_data[splitted[0]] = splitted[1]  # Todo: make more robust.
+        return main_data
 
