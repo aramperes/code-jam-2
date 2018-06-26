3ac63da6bb5f88e052d7e2fb08b8e8b98fe44798
momothereal <momothereal.mc@gmail.com>
2018-06-26 19:48:23 -0400

    Add more documentation everywhere

--- proj/database/mixin.py
+++ proj/database/mixin.py
@@ -5,14 +5,16 @@ from proj.database.database_manager import DatabaseManager
 
 class DatabaseMixin:
     """
-    A mixin to add access to the database from inside resources.
+    A mixin to add access to the database from inside the API resources.
     """
 
     @classmethod
     def setup(cls, web_app):
+        # Create a reference to the DatabaseManager
         cls._db = ref(web_app.db)
         return cls
 
     @property
     def db(self) -> DatabaseManager:
+        # Get the value from the DatabaseManager reference
         return self._db()
