9ae25cbc811b2ae45475ca2de5f03c12bab3bec5
WrongEnd <jacobsroom@hotmail.com>
2018-06-26 05:26:37 -0500

    safeload

--- proj/config_parser.py
+++ proj/config_parser.py
@@ -12,7 +12,7 @@ class Config:
     def load_config(self):
         # Can also be called by itself to reload config.
         with open(self.config_file) as config_stream:
-            self.config = yaml.load(config_stream)
+            self.config = yaml.safe_load(config_stream)
 
     def change_config_file(self, config_file, reload=True):
         # Used to change which config the pointer points to.
