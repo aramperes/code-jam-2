6b3b66d80b8454be8456fe366901f405a52a333e
momothereal <momothereal.mc@gmail.com>
2018-06-27 17:39:48 -0400

    Start work on unit tests

---
+++ tests/fixtures.py
@@ -0,0 +1,48 @@
+import logging
+
+import pytest
+import rethinkdb
+
+from proj.config_parser import Config
+from proj.web.app import WebApp
+
+log = logging.getLogger(__name__)
+
+
+class ConfigShim(Config):
+    def __init__(self):
+        self.config = {
+            "database": {
+                "ip": "127.0.0.1",
+                "port": 28015,
+                "database_name": "temp_test_database"
+            },
+            "debug": True
+        }
+
+
+@pytest.fixture(scope="module")
+def client(request):
+    """
+    Client fixture.
+    """
+    config = ConfigShim()
+
+    # check if rethinkdb temp database already exists
+    db_name = config.get("database", "database_name")
+    with rethinkdb.connect(host=config.get("database", "ip"), port=config.get("database", "port"),
+                           db=config.get("database", "database_name")) as conn:
+        if db_name in rethinkdb.db_list().run(conn):
+            rethinkdb.db_drop(db_name).run(conn)
+            log.warning("Dropped temporary test database {0}".format(db_name))
+
+    web = WebApp(config)
+    test_client = web.app.test_client()
+
+    def teardown():
+        with web.db.connect() as conn:
+            rethinkdb.db_drop(db_name).run(conn)
+            log.info("Dropped temporary test database {0}".format(db_name))
+
+    request.addfinalizer(teardown)
+    return test_client
