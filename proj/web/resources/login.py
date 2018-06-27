4cd1eac8e2d1e35e5527fead5253c6a584f37dba
momothereal <momothereal.mc@gmail.com>
2018-06-27 11:38:47 -0400

    Use secondary username index in login/register

--- proj/web/resources/login.py
+++ proj/web/resources/login.py
@@ -13,25 +13,22 @@ class LoginResource(BaseResource):
 
     def post(self):
         data = request.json or {}
-
         if 'username' not in data or 'password' not in data:
-            return {'success': False, 'reason': 'enter a username and password'}
+            return {'success': False, 'reason': 'Provide a username and password'}
 
-        user_by_username_query = self.db.query("users").filter(
-            {
-                "username": data.get('username')
-            }
-        ).coerce_to("array")
-        users = self.db.run(user_by_username_query)
+        # Makes sure the input credentials are strings
+        username = str(data["username"])
+        password = str(data["password"])
 
+        # Check if the username exists
+        users = self.db.get_all("users", username, index="username")
         if not users:  # this checks if the users array is empty
-            return {'success': False, 'reason': 'No user found. Use the /register endpoint to register for an account.'}
+            return {'success': False, 'reason': 'Unknown username. Use the /register endpoint to create an account.'}
 
         user_document = users[0]
         password_hash = user_document["password_hash"]
 
-        if bcrypt.checkpw(data.get('password').encode(), password_hash.encode()):
-            # create id param for game room
+        if bcrypt.checkpw(password.encode(), password_hash.encode()):
             return {'success': True}
         else:
             return {'success': False, 'reason': 'Incorrect password.'}
